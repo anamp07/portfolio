0243845061028ad847154367ed35573a1c493dfc
Ana María Pantoja <a.m.pantoja@cgiar.org>
2019-12-11 00:50:52 -0500

    Updates

---
+++ precache-manifest.e18dfc9f73b386800ee8c5558be49b19.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0dc5c5e7e125ff0ffea723ca29206155",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "5531cd9e8e5b2bf27b87",
+    "url": "/portfolio/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "67d4125089a6eaf7e645",
+    "url": "/portfolio/static/css/main.f0e7675c.chunk.css"
+  },
+  {
+    "revision": "5531cd9e8e5b2bf27b87",
+    "url": "/portfolio/static/js/2.030a32b8.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/portfolio/static/js/2.030a32b8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "67d4125089a6eaf7e645",
+    "url": "/portfolio/static/js/main.cc428f7e.chunk.js"
+  },
+  {
+    "revision": "16c109c584451046bd84",
+    "url": "/portfolio/static/js/runtime-main.860ade9b.js"
+  },
+  {
+    "revision": "90706387faf76a8c5384226696fd1585",
+    "url": "/portfolio/static/media/Personajes.90706387.png"
+  },
+  {
+    "revision": "28359df250103599e9feb9ce7026dace",
+    "url": "/portfolio/static/media/asfalt-dark.28359df2.png"
+  },
+  {
+    "revision": "f27d4d122d7552e9f3eed435df20caa9",
+    "url": "/portfolio/static/media/bsportfolio.f27d4d12.png"
+  },
+  {
+    "revision": "ccf144264bbb5bbecfaceae723637adc",
+    "url": "/portfolio/static/media/ccafsguide.ccf14426.png"
+  },
+  {
+    "revision": "ffcdcbcc11831fbe8a8d901aedc12c2d",
+    "url": "/portfolio/static/media/clarisa.ffcdcbcc.gif"
+  },
+  {
+    "revision": "0847c3362631b523c2edde35386b1023",
+    "url": "/portfolio/static/media/doumies.0847c336.png"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/portfolio/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "74f205f9b108198c2b056e8562e6b18e",
+    "url": "/portfolio/static/media/profile.74f205f9.png"
+  },
+  {
+    "revision": "16e57541b308623e4eea0a5612380168",
+    "url": "/portfolio/static/media/resultsDashboard.16e57541.png"
+  },
+  {
+    "revision": "ed98547efcc5aa13d71ce1c4dce60230",
+    "url": "/portfolio/static/media/walle.ed98547e.png"
+  }
+]);
