106d4d4ba76d9630603d54554ef73caec951501f
anamp07 <anamaria0711@gmail.com>
2020-07-10 10:02:10 -0500

    Updates

---
+++ precache-manifest.945e5885f3fc86e2ecb0d9780dbc9271.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a47d9afbb1eb4d3630d77ca09b0440e2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c6841ed1071ccf5f3649",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "2b64bb18d47c7427545b",
+    "url": "/static/css/main.d5f30f9b.chunk.css"
+  },
+  {
+    "revision": "c6841ed1071ccf5f3649",
+    "url": "/static/js/2.2245da65.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.2245da65.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b64bb18d47c7427545b",
+    "url": "/static/js/main.10e9af95.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "90706387faf76a8c5384226696fd1585",
+    "url": "/static/media/Personajes.90706387.png"
+  },
+  {
+    "revision": "28359df250103599e9feb9ce7026dace",
+    "url": "/static/media/asfalt-dark.28359df2.png"
+  },
+  {
+    "revision": "f27d4d122d7552e9f3eed435df20caa9",
+    "url": "/static/media/bsportfolio.f27d4d12.png"
+  },
+  {
+    "revision": "ccf144264bbb5bbecfaceae723637adc",
+    "url": "/static/media/ccafsguide.ccf14426.png"
+  },
+  {
+    "revision": "ffcdcbcc11831fbe8a8d901aedc12c2d",
+    "url": "/static/media/clarisa.ffcdcbcc.gif"
+  },
+  {
+    "revision": "0847c3362631b523c2edde35386b1023",
+    "url": "/static/media/doumies.0847c336.png"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "74f205f9b108198c2b056e8562e6b18e",
+    "url": "/static/media/profile.74f205f9.png"
+  },
+  {
+    "revision": "16e57541b308623e4eea0a5612380168",
+    "url": "/static/media/resultsDashboard.16e57541.png"
+  },
+  {
+    "revision": "ed98547efcc5aa13d71ce1c4dce60230",
+    "url": "/static/media/walle.ed98547e.png"
+  }
+]);
